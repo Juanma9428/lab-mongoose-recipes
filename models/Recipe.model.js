2ecd99defc49ed1640c1cf8c050bd364b753bc07
Juan Manuel <j-grafi@hotmail.com>
2024-11-07 17:43:24 +0100

    first interation

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const LEVEL = ['Easy Peasy', 'Chef amateur', 'Chef ultraprofesional'];
+const DISH = [ "desayuno" , "plato principal" , "sopa" , "merienda" , "bebida" , "postre", "otro"]
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type : String,
+    required : [true, 'A recipe needs a title']
+  },
+  level:{
+    type: String,
+    enum: LEVEL
+  },
+  ingredients:{
+    type:[string]
+  },
+  
+  kitchen:{
+    type:string,
+    required : [true, 'A recipe needs a kitchen']
+},
+dishType:{
+  type:string,
+  enum: DISH
+},
+image:{
+  type:string,
+  default: "https://images.media-allrecipes.com/images/75131.jpg"
+
+},
+duration:{
+  type:Number,
+  min: [0, 'At least 0']
+},
+creator:{
+    type:string,
+
+},
+created:{
+  type:Date,
+  default: Date.now()
+}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
